e9bd85a5ad3fc979c7919244eb7d5fdee2ee1861
zs <543322672@qq.com>
2022-07-07 18:28:06 +0800

    main

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import './App.css';
 import React, { Component } from 'react';
 import Exhibit from './Exhibit';
 // import ipify from 'ipify';
-
+import AddressDisplay from './AddressDisplay';
 
 class App extends Component {
   render() {
       return (
           <div className="App">
               <Exhibit name="ipv4,,,,,,,,,,," children="await ipify()">
-              
+              <AddressDisplay url='https://api.ipify.org?format=json' />
               </Exhibit>
               <Exhibit name="IPV6,............" children="await ipify({useIPv6: false})">
-               
+              <AddressDisplay url='https://api64.ipify.org?format=json' />
                 </Exhibit>
           </div>
       );
